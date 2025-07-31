fab62af368fead65e32f47476d8d602294395322
BrunoC30 <bruno01eufonio@gmail.com>
2025-07-31 00:45:00 -0300

    botão 'me identifico' adicionado

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
+//elementos do container hack de estudos
 const obterResposta = document.getElementById("obterResposta");
 const cardHacksMentais = document.getElementById("hacksMentais");
 const novoConteudo = document.getElementById("hacks");
 const trancado = document.getElementById("trancado");
 
+//elementos do botão
+let numero= 6;
+let apertou = false
+const mostrarNumero = document.getElementById("mostrarNumero");
+const btao = document.getElementById("btao");
+
+//ação de liberar o acesso
 obterResposta.addEventListener("input",()=>{
 
     if(obterResposta.value.trim().toLowerCase()==="poder"){
@@ -12,3 +20,16 @@ obterResposta.addEventListener("input",()=>{
         trancado.textContent="🔓Liberado🔓"
     }
 });
+
+//ao apertar o numero aumenta
+btao.addEventListener("click", ()=>{
+    if(apertou==false){
+        numero++;
+        mostrarNumero
+        mostrarNumero.innerText= `${numero} pessoas se identificaram`
+        apertou=true;
+        btao.style.backgroundColor= "#288f64ff";
+    }
+});
+
+mostrarNumero.innerText= `${numero} pessoas se identificaram`
